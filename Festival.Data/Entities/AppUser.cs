c324304baa2c2b9f24d8a927d28cf88bea8266c7
LBK <bakhaipth@gmail.com>
2023-06-01 23:57:46 +0700

    upload

---
+++ Festival.Data/Entities/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FestivalHue.Data.Entities
+{
+    public class AppUser:IdentityUser<Guid>
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime Dob { get; set; }
+    }
+}
---
+++ Project2-FestivalHue/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project2_FestivalHue.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
